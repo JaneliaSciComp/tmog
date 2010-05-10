adb2016ddc5322acd1933660955b6836cb62a178
Eric T. Trautman <trautmane@janelia.hhmi.org>
2010-05-10 18:32:12 +0000

    cache valid ChaCRM line names to prevent TCP socket startvation for large file sets
    git-svn-id: https://subversion.int.janelia.org/ScientificComputing/Projects/IMSImageRenamer/Trunk@4036 86afb187-6117-0410-83e3-87b601379a31

--- src/org/janelia/it/chacrm/ChacrmLineValidator.java
+++ src/org/janelia/it/chacrm/ChacrmLineValidator.java
@@ -1,12 +1,14 @@
 /*
- * Copyright 2007 Howard Hughes Medical Institute.
+ * Copyright (c) 2010 Howard Hughes Medical Institute.
  * All rights reserved.
- * Use is subject to Janelia Farm Research Center Software Copyright 1.0
- * license terms (http://license.janelia.org/license/jfrc_copyright_1_0.html).
+ * Use is subject to Janelia Farm Research Campus Software Copyright 1.1
+ * license terms (http://license.janelia.org/license/jfrc_copyright_1_1.html).
  */
 
 package org.janelia.it.chacrm;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.janelia.it.ims.tmog.config.PluginConfiguration;
 import org.janelia.it.ims.tmog.plugin.ExternalDataException;
 import org.janelia.it.ims.tmog.plugin.ExternalSystemException;
@@ -16,6 +18,8 @@ import org.janelia.it.ims.tmog.plugin.RenamePluginDataRow;
 import org.janelia.it.ims.tmog.plugin.RowValidator;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * This class validates that lines entered with GMR_ prefixes actually
@@ -26,17 +30,49 @@ import java.io.File;
 public class ChacrmLineValidator
         implements RowValidator {
 
+    /** The logger for this class. */
+    private static final Log LOG =
+            LogFactory.getLog(ChacrmLineValidator.class);
+
     /** The data access object for retrieving and updating transformant data. */
     private TransformantDao dao;
 
     /** The name of the property that contains the ChaCRM line. */
-    private String linePropertyName = "Line";
+    private String linePropertyName;
+
+    /**
+     * The maximum amount of time (in milliseconds) between cache
+     * references before the cache should be cleared.  This is intended to
+     * keep the cache from getting stale.
+     */
+    private long clearCacheDuration;
+
+    /** Time the cache was last referenced. */
+    private long lastCacheAccessTime;
+
+    /**
+     * Cache for previously validated transformant identifiers.
+     *
+     * This improves performance but is actually needed to reduce
+     * the number of TCP/IP sockets created for ChaCRM database connections.
+     * Although each connection is properly closed, the OS can leave the
+     * socket in a TIME_WAIT state for up to 4 minutes after the close.
+     * With a large enough file set, all sockets can eventually be taken.
+     * A database connection pool would also resolve this problem.
+     *
+     * TODO: consider adding database connection pooling support
+     */
+    private Set<String> validTransformantIds;
 
     /**
      * Empty constructor required by
      * {@link org.janelia.it.ims.tmog.config.PluginFactory}.
      */
     public ChacrmLineValidator() {
+        this.linePropertyName = "Line";
+        this.clearCacheDuration = 60 * 1000; // one minute
+        this.lastCacheAccessTime = System.currentTimeMillis();
+        this.validTransformantIds = new HashSet<String>();
     }
 
     /**
@@ -56,7 +92,13 @@ public class ChacrmLineValidator
             if (configuredLinePropertyName != null) {
                 this.linePropertyName = configuredLinePropertyName;
             }
-        } catch (ExternalSystemException e) {
+            String configuredClearCacheDuration =
+                    config.getProperty("clearCacheDuration");
+            if (configuredClearCacheDuration != null) {
+                this.clearCacheDuration =
+                        Long.parseLong(configuredClearCacheDuration);
+            }
+        } catch (Exception e) {
             throw new ExternalSystemException(
                     "Failed to initialize ChacrmLineValidator plugin.  " +
                     e.getMessage(),
@@ -80,27 +122,34 @@ public class ChacrmLineValidator
             throws ExternalDataException, ExternalSystemException {
 
         RenamePluginDataRow dataRow = PluginUtil.castRenameRow(row, this);
-        String transformantID = null;
-        try {
-            transformantID = getTransformantID(dataRow);
-            if (transformantID != null) {
-                dao.getTransformant(transformantID, false);
-            }
-        } catch (TransformantNotFoundException e) {
-            File fromFile = dataRow.getFromFile();
-            String fileName = fromFile.getName();
-            throw new ExternalDataException(
-                    "Transformant ID '" + transformantID +
-                    "' does not exist in the ChaCRM database.  " +
-                    "Please verify the " + linePropertyName +
-                    " specified for the file " +
-                    fileName, e);
-        } catch (ExternalSystemException e) {
-            throw new ExternalSystemException(
-                    "Failed to retrieve ChaCRM status for transformant ID '" +
-                    transformantID + "' because of a system error.", e);
-        }
 
+        String transformantId = getTransformantID(dataRow);
+        if (transformantId != null) {
+
+            clearCacheIfStale();
+
+            boolean isValid = validTransformantIds.contains(transformantId);
+            if (! isValid) {
+                try {
+                    dao.getTransformant(transformantId, false);
+                    addTransformantIdToCache(transformantId);
+                } catch (TransformantNotFoundException e) {
+                    File fromFile = dataRow.getFromFile();
+                    String fileName = fromFile.getName();
+                    throw new ExternalDataException(
+                            "Transformant ID '" + transformantId +
+                            "' does not exist in the ChaCRM database.  " +
+                            "Please verify the " + linePropertyName +
+                            " specified for the file " +
+                            fileName, e);
+                } catch (ExternalSystemException e) {
+                    throw new ExternalSystemException(
+                            "Failed to retrieve ChaCRM status for " +
+                            "transformant ID '" + transformantId +
+                            "' because of a system error.", e);
+                }
+            }
+        }
     }
 
     /**
@@ -132,4 +181,28 @@ public class ChacrmLineValidator
         }
     }
 
+    /**
+     * Clears the cache if it has not been accessed recently.
+     */
+    private synchronized void clearCacheIfStale() {
+
+        if ((System.currentTimeMillis() - lastCacheAccessTime) >
+            clearCacheDuration) {
+
+            LOG.info("clearing cache containing " +
+                     validTransformantIds.size() + " items");
+            validTransformantIds.clear();
+        }
+
+        lastCacheAccessTime = System.currentTimeMillis();
+    }
+
+    /**
+     * Adds the specified transformant identifier to the cache.
+     *
+     * @param  transformantId  identifier to add.
+     */
+    private synchronized void addTransformantIdToCache(String transformantId) {
+        validTransformantIds.add(transformantId);
+    }
 }
---
+++ test/org/janelia/it/chacrm/ChacrmLineValidatorTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2010 Howard Hughes Medical Institute.
+ * All rights reserved.
+ * Use is subject to Janelia Farm Research Campus Software Copyright 1.1
+ * license terms (http://license.janelia.org/license/jfrc_copyright_1_1.html).
+ */
+
+package org.janelia.it.chacrm;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.janelia.it.ims.tmog.DataRow;
+import org.janelia.it.ims.tmog.config.PluginConfiguration;
+import org.janelia.it.ims.tmog.field.VerifiedTextModel;
+import org.janelia.it.ims.tmog.plugin.ExternalDataException;
+import org.janelia.it.ims.tmog.plugin.RenamePluginDataRow;
+import org.janelia.it.ims.tmog.target.FileTarget;
+
+import java.io.File;
+
+/**
+ * Tests the ChacrmLineValidator class.
+ *
+ * @author Eric Trautman
+ */
+public class ChacrmLineValidatorTest
+        extends TestCase {
+
+    /**
+     * Constructs a test case with the given name.
+     *
+     * @param  name  name of the test case.
+     */
+    public ChacrmLineValidatorTest(String name) {
+        super(name);
+    }
+
+    /**
+     * Static method to return a suite of all tests.
+     * <p/>
+     * The JUnit framework uses Java reflection to build a suite of all public
+     * methods that have names like "testXXXX()".
+     *
+     * @return suite of all tests defined in this class.
+     */
+    public static Test suite() {
+        return new TestSuite(ChacrmLineValidatorTest.class);
+    }
+
+    /**
+     * Tests the validate method.
+     *
+     * @throws Exception
+     *   if any unexpected errors occur.
+     */
+    public void testValidate() throws Exception {
+
+        final PluginConfiguration pluginConfig = new PluginConfiguration();
+        ChacrmLineValidator validator = new ChacrmLineValidator();
+        validator.init(pluginConfig);
+
+        VerifiedTextModel field = new VerifiedTextModel();
+        field.setDisplayName("Line");
+        field.setText("GMR_15E08_AE_01");
+        DataRow dataRow = new DataRow(new FileTarget(new File("foo")));
+        dataRow.addField(field);
+        RenamePluginDataRow row = new RenamePluginDataRow(new File("bar"),
+                                                          dataRow,
+                                                          new File("."));
+
+        final long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 3000; i++) {
+            validator.validate(row);
+            if ((System.currentTimeMillis() - startTime) > 5000) {
+                fail("Cached transformant id checks are taking too long.");
+            }
+        }
+
+        field.setText("GMR_Bad_Name");
+        try {
+            validator.validate(row);
+            fail("invalid transformant id should have caused data exception");
+        } catch (ExternalDataException e) {
+            // test passed!
+        }
+    }
+}
