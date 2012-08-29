08b29dc54b1512d7666e1d73167b424049310167
Eric T. Trautman <trautmane@janelia.hhmi.org>
2012-08-29 17:15:35 +0000

    add date support to target property default values
    git-svn-id: https://subversion.int.janelia.org/ScientificComputing/Projects/IMSImageRenamer/Trunk@12641 86afb187-6117-0410-83e3-87b601379a31

--- src/org/janelia/it/ims/tmog/field/TargetPropertyDefaultValue.java
+++ src/org/janelia/it/ims/tmog/field/TargetPropertyDefaultValue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 Howard Hughes Medical Institute.
+ * Copyright (c) 2012 Howard Hughes Medical Institute.
  * All rights reserved.
  * Use is subject to Janelia Farm Research Campus Software Copyright 1.1
  * license terms (http://license.janelia.org/license/jfrc_copyright_1_1.html).
@@ -7,8 +7,12 @@
 
 package org.janelia.it.ims.tmog.field;
 
+import org.apache.log4j.Logger;
 import org.janelia.it.ims.tmog.target.Target;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +28,9 @@ public class TargetPropertyDefaultValue
 
     private String propertyName;
     private Map<String, String> map;
+    private SimpleDateFormat fromDateFormatter;
+    private SimpleDateFormat toDateFormatter;
+
 
     @SuppressWarnings({"UnusedDeclaration"})
     public TargetPropertyDefaultValue() {
@@ -42,15 +49,57 @@ public class TargetPropertyDefaultValue
         map.put(mappedValue.getFrom(), mappedValue.getTo());
     }
 
+    public String getFromDatePattern() {
+        return fromDateFormatter.toPattern();
+    }
+
+    @SuppressWarnings({"UnusedDeclaration"})
+    public void setFromDatePattern(String fromDatePattern) {
+        this.fromDateFormatter = new SimpleDateFormat(fromDatePattern);
+        this.fromDateFormatter.setLenient(false);
+    }
+
+    @SuppressWarnings({"UnusedDeclaration"})
+    public String getToDatePattern() {
+        return toDateFormatter.toPattern();
+    }
+
+    @SuppressWarnings({"UnusedDeclaration"})
+    public void setToDatePattern(String toDatePattern) {
+        this.toDateFormatter = new SimpleDateFormat(toDatePattern);
+    }
+
     public String getValue(Target target) {
         String value = target.getProperty(propertyName);
+
         if (map != null) {
+
             String mappedValue = map.get(value);
             if (mappedValue != null) {
                 value = mappedValue;
             }
+
+        } else if ((fromDateFormatter != null) &&
+                   (toDateFormatter != null) &&
+                   (value != null)) {
+
+            try {
+                Date date = fromDateFormatter.parse(value);
+                value = toDateFormatter.format(date);
+            } catch (ParseException e) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("skipping reformat of default date, " +
+                              "failed to parse '" + value +
+                              "' with date format " + getFromDatePattern());
+                }
+            }
         }
+
         return value;
     }
+
+    /** The logger for this class. */
+    private static final Logger LOG =
+            Logger.getLogger(TargetPropertyDefaultValue.class);
 
 }
