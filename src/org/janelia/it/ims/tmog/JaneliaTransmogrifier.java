4276b930f215010974f81dd9510b43637415af71
Eric T. Trautman <trautmane@janelia.hhmi.org>
2013-07-15 18:32:05 +0000

    add slide code setter plugin for leet lab
    git-svn-id: https://subversion.int.janelia.org/ScientificComputing/Projects/IMSImageRenamer/Trunk@16620 86afb187-6117-0410-83e3-87b601379a31

--- src/org/janelia/it/ims/tmog/JaneliaTransmogrifier.java
+++ src/org/janelia/it/ims/tmog/JaneliaTransmogrifier.java
@@ -42,7 +42,7 @@ public class JaneliaTransmogrifier extends JFrame {
      * app is run from within an IDE (package information can be used
      * when the app is run from a jar file). 
      */
-    public static final String VERSION = "3.1.1";
+    public static final String VERSION = "3.1.2";
     
     /**
      * Set up a thread pool to limit the number of concurrent
