b1d2dcf76436ca42d325aec097ee74d9f7f2bd74
Eric T. Trautman <trautmane@janelia.hhmi.org>
2017-01-13 20:06:16 +0000

    fix collector view bug that forced all collectors to be sage collectors
    git-svn-id: https://subversion.int.janelia.org/ScientificComputing/Projects/IMSImageRenamer/Trunk@20143 86afb187-6117-0410-83e3-87b601379a31

--- src/org/janelia/it/ims/tmog/JaneliaTransmogrifier.java
+++ src/org/janelia/it/ims/tmog/JaneliaTransmogrifier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Howard Hughes Medical Institute.
+ * Copyright (c) 2017 Howard Hughes Medical Institute.
  * All rights reserved.
  * Use is subject to Janelia Farm Research Campus Software Copyright 1.1
  * license terms (http://license.janelia.org/license/jfrc_copyright_1_1.html).
@@ -44,7 +44,7 @@ public class JaneliaTransmogrifier extends JFrame implements ConfigurationLoadCo
      * app is run from within an IDE (package information can be used
      * when the app is run from a jar file). 
      */
-    public static final String VERSION = "4.1.9";
+    public static final String VERSION = "4.2.0";
     
     /**
      * Set up a thread pool to limit the number of concurrent
